1b1bf26c7eda9924ba4513a25f886c48e167ffe5
gkwhdcks9 <137044703+gkwhdcks9@users.noreply.github.com>
2024-02-18 17:56:40 +0900

    프런트 단 javascript 파일 만들기

--- app/app.js
+++ app/app.js
@@ -11,6 +11,7 @@ const home=require("./src/routes/home");
 //앱세팅
 app.set("views","./src/views");
 app.set("view engine","ejs");
+app.use(express.static(`${__dirname}/src/public`));//__dirname : app.js의 위치를 반환
 
 
 app.use("/", home);//use -> 미들웨어를 등록해주는 메서드
