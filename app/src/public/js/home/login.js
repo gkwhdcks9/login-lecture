26f0f7d639ff3db527b85bb8a6db40127900dd72
gkwhdcks9 <137044703+gkwhdcks9@users.noreply.github.com>
2024-02-22 16:55:46 +0900

    프런트 로그인 기능 구현

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -19,6 +19,14 @@ function login(){
         body: JSON.stringify(req)//body를 통해서 데이터 전달할 때는 http메서드 중 POST를 써서 전달
     })
     .then((res) => res.json())
-    .then((res) => console.log(res));
-    
+    .then((res) => {
+        if(res.success){
+            location.href = "/";
+        } else {
+            alert(res.msg);
+        }                
+    })
+    .catch((err) => {
+        console.error(new Error("로그인 중 에러 발생"));
+    });  
 }
