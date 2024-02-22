76d1e9704e644bf68f21081941e87da7e67695da
gkwhdcks9 <137044703+gkwhdcks9@users.noreply.github.com>
2024-02-22 15:13:09 +0900

    DOM을 이용하여 HTML 객체 저장

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,4 +1,15 @@
 "use strict";
 
-console.log("hello");
-console.log("bye");
+const id = document.querySelector("#id"), // 태그에 id = "identity"에서 identity를 가져와 달라.
+    psword = document.querySelector("#psword"),
+    loginBtn = document.querySelector("button");
+loginBtn.addEventListener("click", login);
+
+function login(){
+    const req = {
+        id: id.value,
+        psword: psword.value,
+    };
+    console.log(req);
+    
+}
